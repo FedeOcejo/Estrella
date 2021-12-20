141bd13f6bbacadfd376e4a68383d54fa9473019
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-12-20 17:16:54 +0100

    final estrella

--- Estrella.py
+++ Estrella.py
@@ -4,14 +4,8 @@ ws = turtle.Screen()
 
 geekyTurtle = turtle.Turtle()
 
-for i in range(5):
+for i in range(9):
 
     geekyTurtle.forward(100)
 
-    geekyTurtle.right(144)
-
-for i in range(8):
-
-    geekyTurtle.forward(100)
-
-    geekyTurtle.right(157.5)
+    geekyTurtle.right(160)
